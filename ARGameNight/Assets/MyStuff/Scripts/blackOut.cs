a49bbed0b27533452250cf277e5ddc3b84cc4d13
Daniel Bramwell <dbramwell@ufl.edu>
2019-08-28 16:51:49 -0400

    Uploading project to repo

---
+++ ARGameNight/Assets/MyStuff/Scripts/blackOut.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class blackOut : MonoBehaviour {
+
+    public static Color[] colors = { Color.green, Color.red, Color.blue, Color.yellow };
+    public static string[] shapes = { "square", "circle", "triangle", "waves", "crown" };
+    public static bool shuffled = false;
+    public static int randomNum;
+
+    // Use this for initialization
+    void Start () {
+        shuffled = false;
+        shuffleShapes();
+        randomNum = Random.Range(0, 4);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public static void shuffleShapes()
+    {
+        for (int t = 0; t < shapes.Length; t++)
+        {
+            string tmp = shapes[t];
+            int r = Random.Range(t, shapes.Length);
+            shapes[t] = shapes[r];
+            shapes[r] = tmp;
+        }
+        Debug.Log("Shuffled Shapes Complete:\n" + shapes);
+        shuffled = true;
+    }
+
+}
---
+++ ARGameNight/Assets/MyStuff/Scripts/shape.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shape : MonoBehaviour {
+
+    Color color;
+
+    // Use this for initialization
+    void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (blackOut.shuffled)
+        {
+            for (int i = 0; i < blackOut.shapes.Length; i++)
+            {
+                if (blackOut.shapes[i] == this.tag)
+                {
+                    if (i > 3)
+                    {
+                        color = blackOut.colors[blackOut.randomNum];
+                        GetComponent<Renderer>().material.color = color;
+                        Debug.Log("Set " + blackOut.shapes[i] + " to " + color);
+                        break;
+                    }
+
+                    color = blackOut.colors[i];
+                    GetComponent<Renderer>().material.color = color;
+                    Debug.Log("Set " + blackOut.shapes[i] + " to " + color);
+                    break;
+                }
+            }
+        }
+    }
+}
